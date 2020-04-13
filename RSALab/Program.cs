f9f5bcd0fd66698032ce309c89067b344bf8f2c9
SachekDenis <sachek.denis@gmail.com>
2020-04-13 08:50:58 +0300

    dsfssfs

--- RSALab/Program.cs
+++ RSALab/Program.cs
@@ -6,11 +6,14 @@ namespace RSALab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Начальная строка: сачек денис николаевич");
             RsaEncoder encoder = new RsaEncoder();
             var keys = encoder.GenerateKeys(3, 19);
             var encodedMessage = encoder.Encode("сачек денис николаевич", keys);
+            Console.WriteLine($"Открытый ключ: {keys.EOpenKey} , {keys.NOpenKey}. Секретный ключ: {keys.DSecretKey} , {keys.NSecretKey}");
+            Console.WriteLine("Зашифрованая строка:" + encodedMessage);
             var decodedMessage = encoder.Decode(encodedMessage, keys);
+            Console.WriteLine("Расшифрованная строка:" + decodedMessage);
         }
     }
 }
